b4f9ed408f2dd0f38b1d2ec390f729a971a75217
moj124 <michael.jowen.jones@gmail.com>
2024-06-26 22:40:13 +0100

    fix syntax

--- src/chat/chat.gateway.ts
+++ src/chat/chat.gateway.ts
@@ -1,4 +1,4 @@
-import { 
+import {
   SubscribeMessage,
   WebSocketGateway,
   WebSocketServer,
@@ -7,9 +7,9 @@ import {
   OnGatewayDisconnect,
 } from '@nestjs/websockets';
 import { Logger } from '@nestjs/common';
-import { Server, Socket} from 'socket.io';
+import { Server, Socket } from 'socket.io';
 
-import { AddMessageDto} from '../dto/addMessage.dto';
+import { AddMessageDto } from '../dto/addMessage.dto';
 
 @WebSocketGateway({ cors: { origin: '*' } })
 export class ChatGateway implements OnGatewayConnection, OnGatewayDisconnect {
@@ -20,7 +20,7 @@ export class ChatGateway implements OnGatewayConnection, OnGatewayDisconnect {
 
   @SubscribeMessage('chat')
   handleMessage(@MessageBody() payload: AddMessageDto): AddMessageDto {
-    this.logger.log(`Message received: ${payload.userId} - ${payload.body}`);
+    this.logger.log(`Message received: ${payload.id} - ${payload.userId} - ${payload.body}`);
     this.server.emit('chat', payload);
     return payload;
   }
--- src/dto/addMessage.dto.ts
+++ src/dto/addMessage.dto.ts
@@ -1,4 +1,6 @@
 export class AddMessageDto {
+    id: number;
+
     userId: number;
 
     body: Object;
